919188fbf98d879a3000d74b4efd525156843249
Maqsud <maqsud13101033@gmail.com>
2020-05-09 23:57:49 +0600

    Updates

---
+++ precache-manifest.6e6fed1ace17f44690d0be18eba75ae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7a5037268902d24b7d6b451a4d5551",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "edf4119a159fdfa5ed56",
+    "url": "/my-profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c28a4a6f0052d7f62064",
+    "url": "/my-profile/static/js/2.7a963b43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-profile/static/js/2.7a963b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf4119a159fdfa5ed56",
+    "url": "/my-profile/static/js/main.c69cdb4b.chunk.js"
+  },
+  {
+    "revision": "81603062695ff432570e",
+    "url": "/my-profile/static/js/runtime-main.b4e62dfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
